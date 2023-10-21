9097bbdd8d72b786784a8ebd0ce9e09244a08ec9
Fidias <fidiasierides@gmail.com>
2023-10-21 19:56:31 +0100

    milestone_3

---
+++ hangman/milestone_3.py
@@ -0,0 +1,18 @@
+from milestone_2 import word
+
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+         print(f"Sorry, {guess} is not in the word. Try again.")
+def ask_for_input():
+    while True:
+        guess = input('Enter a letter: ')
+        if len(str(guess)) == 1 and guess.isalpha():
+            break
+        else:
+            print('Invalid letter. Please, enter a single alphabetical character.')
+    check_guess(guess)
+
+ask_for_input()
